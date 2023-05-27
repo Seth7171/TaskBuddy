560ee5e200ce75a44ddef268622335b99e8f8084
Seth7171 <ronshahar6@gmail.com>
2023-05-27 16:44:25 +0300

    added validation to the signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,8 +18,20 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, password) {
-    const exists = await this.findOne({ email })
 
+    // validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)){
+        throw Error('Password is not strong enough')
+    }
+
+    // checking if we have this email in the db
+    const exists = await this.findOne({ email })
     if (exists){
         throw Error('Email already in use')
     }
