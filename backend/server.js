90a9c1013213c074fcd6e12a0cf29c6bf38c57b2
Seth7171 <ronshahar6@gmail.com>
2023-05-27 16:10:19 +0300

    added backend first user models

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // clear    To clear the terminal
 // cancle process in terminal : ctrl + c
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
