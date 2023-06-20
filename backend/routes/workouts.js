1109b597faf24544d9d9a07a270135593504fa4a
Nitsan.Maman <nit.maman@gmail.com>
2023-06-20 14:55:26 +0300

    authorized Request

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,7 +6,11 @@ const {
     deleteWorkout,
     updateWorkout
 } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
+
 const router = express.Router()
+
+router.use(requireAuth)
 
 //get all workouts
 router.get('/', getWorkouts)
