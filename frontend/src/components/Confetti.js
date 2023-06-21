027ea85dde2d3e6754e130a510cf66d6d7bed4ab
Seth7171 <ronshahar6@gmail.com>
2023-06-21 17:49:13 +0300

    added fun

--- frontend/src/components/Confetti.js
+++ frontend/src/components/Confetti.js
@@ -1,70 +1,65 @@
-import React from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+
 import ReactCanvasConfetti from 'react-canvas-confetti';
 
-const canvasStyles = {
-  position: 'fixed',
-  pointerEvents: 'none',
-  width: '100%',
-  height: '100%',
-  top: 0,
-  left: 0
-};
+export default function Confetti() {
+  const refAnimationInstance = useRef(null);
 
-class Confetti extends React.Component {
-  constructor(props) {
-    super(props);
-    this.animationInstance = null;
-  }
+  const getInstance = useCallback(instance => {
+    refAnimationInstance.current = instance;
+  }, []);
 
-  makeShot = (particleRatio, opts) => {
-    this.animationInstance && this.animationInstance({
-      ...opts,
-      origin: { y: 0.7 },
-      particleCount: Math.floor(200 * particleRatio),
-    });
-  }
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
 
-  fire = () => {
-    this.makeShot(0.25, {
+  useEffect(() => fire(), []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
       spread: 26,
-      startVelocity: 55,
+      startVelocity: 55
     });
-    this.makeShot(0.2, {
-      spread: 60,
+
+    makeShot(0.2, {
+      spread: 60
     });
-    this.makeShot(0.35, {
+
+    makeShot(0.35, {
       spread: 100,
       decay: 0.91,
-      scalar: 0.8,
+      scalar: 0.8
     });
-    this.makeShot(0.1, {
+
+    makeShot(0.1, {
       spread: 120,
       startVelocity: 25,
       decay: 0.92,
-      scalar: 1.2,
+      scalar: 1.2
     });
-    this.makeShot(0.1, {
+
+    makeShot(0.1, {
       spread: 120,
-      startVelocity: 45,
+      startVelocity: 45
     });
-  }
+  }, [makeShot]);
 
-  handlerFire = () => {
-    this.fire();
-  };
-
-  getInstance = (instance) => {
-    this.animationInstance = instance;
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handlerFire}>fire</button>
-        <ReactCanvasConfetti refConfetti={this.getInstance} style={canvasStyles} />
-      </div>
-    )
-  }
+  return (
+    <ReactCanvasConfetti
+      refConfetti={getInstance}
+      style={{
+        position: 'fixed',
+        pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0
+      }}
+    />
+  );
 }
-
-export default Confetti;
--- frontend/src/components/TaskDetails.js
+++ frontend/src/components/TaskDetails.js
@@ -1,29 +1,36 @@
-
 import { useTasksContext } from "../hooks/useTasksContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { format } from "date-fns";
+import { useState } from 'react';
+import Confetti from './Confetti';
 
 const TaskDetails = ({ task }) => {
   const { dispatch } = useTasksContext();
   const { user } = useAuthContext();
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleClick = async () => {
     if (!user) {
       return;
     }
+
     const response = await fetch("/api/tasks/" + task._id, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${user.token}`,
       },
     });
+
     const json = await response.json();
 
     if (response.ok) {
       dispatch({ type: "DELETE_TASK", payload: json });
     }
+  };
 
-
+  const handleRadioButtonClick = () => {
+    setIsVisible(true); // Set isVisible to true after deleting the task
+    handleClick(); // Call handleClick when the radio button is clicked
   };
 
   const formattedDeadline = format(new Date(task.deadline), "MMMM dd, yyyy");
@@ -43,9 +50,10 @@ const TaskDetails = ({ task }) => {
         <strong>Deadline: </strong>
         {formattedDeadline}
       </p>
-        <span className="material-symbols-outlined" onClick={handleClick}>
-          radio_button_unchecked
-        </span>
+      <span className="material-symbols-outlined" onClick={handleRadioButtonClick}>
+        radio_button_unchecked
+      </span>
+      {isVisible && <Confetti />}
     </div>
   );
 };
