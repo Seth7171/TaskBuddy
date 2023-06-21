d35caf3dbe15438b26424708c3358a96460524da
Nitsan.Maman <nit.maman@gmail.com>
2023-06-21 21:39:12 +0300

    confetti fix

--- frontend/src/components/TaskDetails.js
+++ frontend/src/components/TaskDetails.js
@@ -29,32 +29,38 @@ const TaskDetails = ({ task }) => {
   };
 
   const handleRadioButtonClick = () => {
-    setIsVisible(true); // Set isVisible to true after deleting the task
-    handleClick(); // Call handleClick when the radio button is clicked
+    setIsVisible(true); // Set isVisible to true when the radio button is clicked
+    setTimeout(() => {
+      handleClick(); // Call handleClick to delete the task
+    }, 5000);
   };
 
   const formattedDeadline = format(new Date(task.deadline), "MMMM dd, yyyy");
 
   return (
-    <div className="task-details">
-      <h4>{task.title}</h4>
-      <p>
-        <strong>Note: </strong>
-        {task.note}
-      </p>
-      <p>
-        <strong>Type: </strong>
-        {task.type}
-      </p>
-      <p>
-        <strong>Deadline: </strong>
-        {formattedDeadline}
-      </p>
-      <span className="material-symbols-outlined" onClick={handleRadioButtonClick}>
-        radio_button_unchecked
-      </span>
+    <>
       {isVisible && <Confetti />}
-    </div>
+      {!isVisible && (
+        <div className="task-details">
+          <h4>{task.title}</h4>
+          <p>
+            <strong>Note: </strong>
+            {task.note}
+          </p>
+          <p>
+            <strong>Type: </strong>
+            {task.type}
+          </p>
+          <p>
+            <strong>Deadline: </strong>
+            {formattedDeadline}
+          </p>
+          <span className="material-symbols-outlined" onClick={handleRadioButtonClick}>
+            radio_button_unchecked
+          </span>
+        </div>
+      )}
+    </>
   );
 };
 
