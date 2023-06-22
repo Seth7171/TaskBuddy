8bbb2f8948af18580c933b7060804422afeecbed
Nitsan.Maman <nit.maman@gmail.com>
2023-06-22 13:32:43 +0300

    exit before delete fix

--- frontend/src/context/TaskContext.js
+++ frontend/src/context/TaskContext.js
@@ -3,36 +3,36 @@ import { createContext, useReducer } from "react";
 export const TasksContext = createContext()
 
 export const tasksReducer = (state, action) => {
-    switch (action.type){
-        case 'SET_TASKS':
-            return {
-                tasks: action.payload
-            }
-
-        case 'CREATE_TASK':
-            return {
-                tasks: [action.payload, ...state.tasks]
-            }
-        
-        case 'DELETE_TASK':
-            return {
-                tasks: state.tasks.filter((w) => w._id !== action.payload._id)
-            }
-            
-        default:
-            return state
+    switch (action.type) {
+      case 'SET_TASKS':
+        return {
+          tasks: action.payload || state.tasks // Handle initial state if payload is null
+        };
+  
+      case 'CREATE_TASK':
+        return {
+          tasks: [action.payload, ...state.tasks]
+        };
+  
+      case 'DELETE_TASK':
+        return {
+          tasks: state.tasks.filter((w) => w._id !== action.payload._id)
+        };
+  
+      default:
+        return state;
     }
-}
-
-
-export const TasksContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(tasksReducer,{
-        tasks: null
-    })
-
+  };
+  
+  export const TasksContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(tasksReducer, {
+      tasks: [] // Set initial state with an empty array
+    });
+  
     return (
-        <TasksContext.Provider value = {{...state, dispatch}}>
-            { children }
-        </TasksContext.Provider>
-    )
-}
+      <TasksContext.Provider value={{ ...state, dispatch }}>
+        {children}
+      </TasksContext.Provider>
+    );
+};
+  
